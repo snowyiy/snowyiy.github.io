2d2e45172c0139b790f0cd7f0cb5f56e554c2998
snowyiy <144264800+snowyiy@users.noreply.github.com>
2023-12-16 23:31:35 +0000

    ajout de la redirection si le mot saisie est correct

--- javascript/script.js
+++ javascript/script.js
@@ -1,10 +1,11 @@
 const motApplication = "password"
 
-let motUtilisateur = prompt("Entrez un mot : " + motApplication)
+let motUtilisateur = prompt("Entrez un mot : ")
 console.log(motUtilisateur)
 
 if (motUtilisateur === motApplication) {
     console.log("Bravo, vous avez correctement tapé le mot")
+    window.location.href = 'https://snowyiy.github.io/accueil.html';
 } else {
     console.log("Échec, le mot n'est pas correct")
 }
